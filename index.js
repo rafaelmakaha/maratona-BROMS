8ebb0e3828bf5b67802ff147a2525d40965a7781
Rafael Makaha <rafael.makaha@gmail.com>
2021-03-07 20:52:20 -0300

    Add resize event listener.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import Scoreboard from './models/Scoreboard.js';
-import Row from './models/Row.js'
 import { getContest } from './services/api.js'
 
 const canvas = document.getElementById("canvas")
@@ -8,7 +7,7 @@ canvas.height = window.innerHeight;
 
 const FILE_SEPARATOR = String.fromCharCode(28);
 
-(async () => {
+const main = async () => {
   const rawData = await getContest('./sample/contest')
   // Instatiate Scoreboard
   const scoreboard = new Scoreboard(canvas, 13)
@@ -23,4 +22,12 @@ const FILE_SEPARATOR = String.fromCharCode(28);
   // c.strokeRect(0,0,100,100)
   // c.strokeRect(200,50,100,100)
   // c.fillText('teste',200,50)
-})()
+}
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  main()
+})
+
+main()
