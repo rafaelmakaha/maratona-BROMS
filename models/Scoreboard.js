cabb7688c2319443c8b9be956754d593bf7d2557
Rafael Makaha <rafael.makaha@gmail.com>
2021-03-07 12:14:55 -0300

    Add addRow, update draw methods and add new values.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- models/Scoreboard.js
+++ models/Scoreboard.js
@@ -1,19 +1,28 @@
+import Row from './Row.js'
+
 class Scoreboard {
-  constructor(rows, canvas, font="30px Comic Sans MS" ) {
-    this.rows = rows;
+  constructor(canvas, totalProblems, font="30px Comic Sans MS" ) {
+    this.rows = new Array(1)
     this.context = canvas.getContext('2d');
+    this.totalProblems = totalProblems;
+    this.totalRows = 0;
+    this.rowHeight = 40;
+    this.rowWidth = 0.9 * window.innerWidth;
     this.font = font;
-    console.log('zeca')
+    this.x = 20;
+    this.y = 20;
   }
-  drawRows() {
-    this.context.beginPath();
+  draw() {
     this.context.fillStyle = 'red';
-    this.context.textAlign = "center";
+    this.context.textAlign = "left";
     this.context.font = this.font;
-    this.context.fillText(this.rows.map((row) => {
-      return row.teamName, row.positionX, row.positionY
-    }));
+    this.rows.slice(1,this.totalRows+1).map((row) => row.draw(this.context))
   }
+  addRow(uid, college, teamName){
+    this.totalRows++;
+    this.rows.push(new Row(this, this.totalRows, uid, college, teamName, this.x, this.y + (this.totalRows * this.rowHeight), this.totalProblems));
+  }
+  notify(position){}
 }
 
 export default Scoreboard;
