eea81f9deab57f3212467ccac789e491babadcc2
Rafael Makaha <rafael.makaha@gmail.com>
2021-03-07 15:08:19 -0300

    Update contest parser.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Scoreboard from './models/Scoreboard.js';
 import Row from './models/Row.js'
-import { getTeams } from './services/api.js'
+import { getContest } from './services/api.js'
 
 const canvas = document.getElementById("canvas")
 canvas.width = window.innerWidth;
@@ -9,18 +9,18 @@ canvas.height = window.innerHeight;
 const FILE_SEPARATOR = String.fromCharCode(28);
 
 (async () => {
-  const rawData = await getTeams('./sample/contest')
+  const rawData = await getContest('./sample/contest')
   // Instatiate Scoreboard
   const scoreboard = new Scoreboard(canvas, 13)
 
   // Instatiate teams
-  rawData.map((team, index) => {
+  rawData.teams.map((team, index) => {
     let [ref, college, teamName] = team.split(FILE_SEPARATOR)
     scoreboard.addRow(ref, college, teamName)
   })
-  // scoreboard.draw()
-  const c = canvas.getContext('2d')
-  c.strokeRect(0,0,100,100)
-  c.strokeRect(200,50,100,100)
-  c.fillText('teste',200,50)
+  scoreboard.draw()
+  // const c = canvas.getContext('2d')
+  // c.strokeRect(0,0,100,100)
+  // c.strokeRect(200,50,100,100)
+  // c.fillText('teste',200,50)
 })()
--- services/api.js
+++ services/api.js
@@ -1,8 +1,17 @@
-export const getTeams = (url) => {
+const FILE_SEPARATOR = String.fromCharCode(28);
+
+export const getContest = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
     .then(response => response.text())
-    .then(text => text.split('\n').slice(3,75))
+    .then(text => { 
+      text = text.split('\n')
+      const [eventTitle, eventInfo, values] = text
+      const [qtdTeams, qtdQuestions] = values.split(FILE_SEPARATOR)
+      text = text.slice(3)
+      const teams = text.slice(0,qtdTeams)
+      return {eventTitle, eventInfo, values, qtdTeams, qtdQuestions, teams}
+    })
     .then(resolve)
     .catch(reject)
   })
