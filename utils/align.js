c3bf71ca141a74693470198998e41a0dbc2f0fa1
Rafael Makaha <rafael.makaha@gmail.com>
2021-03-26 20:25:49 -0300

    Add offset on align method.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- utils/align.js
+++ utils/align.js
@@ -6,10 +6,11 @@ export function align(text, type, width='', height='', font=30) {
   const textMetrics = ctx.measureText(text)
   const text_w = textMetrics.width
   const text_h = textMetrics.actualBoundingBoxAscent
+  const offset = Math.max(width * 0.02, 10);
   const ans = {
     center: [(width - text_w)/2, (height - text_h)/2],
-    left: [0,(height - text_h)/2],
-    right: [width - text_w, (height - text_h)/2],
+    left: [offset,(height - text_h)/2],
+    right: [width - text_w - offset < 0 ? width - text_w : width - text_w - offset, (height - text_h)/2],
   }
   if (text_w > width) return align(text, type, width, height, font-1)
   return ans[type]
