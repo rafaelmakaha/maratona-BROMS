3839f3feca86a4e6a6cf1892b584d11f100869aa
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2022-09-07 18:02:20 -0300

    Changing default api port and mode

--- appSettings.js
+++ appSettings.js
@@ -2,11 +2,11 @@ const MODES = {
     0:  "MOCK",
     1:  "CF",
 }
-export const MODE = MODES[1];
+export const MODE = MODES[0];
 
 // Mock API
 
-export const MOCK_BASE_URL  = 'http://localhost:8002'
+export const MOCK_BASE_URL  = 'http://localhost:8000'
 
 // Codeforces API
 
